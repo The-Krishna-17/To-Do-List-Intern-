ce4029f0723640b72d536c8da0e95b4256b50954
Krishna Dhami <thekrishnadhami17@gmail.com>
2025-01-10 16:56:05 +0545

    adding comments

--- src/ToDo.jsx
+++ src/ToDo.jsx
@@ -1,23 +1,28 @@
 import React, { useState, useEffect } from "react";
+// importing react icon 
 import { IoIosCheckbox } from "react-icons/io";
+// importing custom css
 import "./Todo.css";
 
 const ToDo = () => {
-  const [tasks, setTasks] = useState([]);
-  const [NewTask, setNewTask] = useState("");
-  const [completedTasks, setCompletedTasks] = useState([]);
-  const [inProgress, setInProgress] = useState(0);
-  const [countCompleted, setCountCompleted] = useState(0);
+  const [tasks, setTasks] = useState([]); //empty array to store added task
+  const [NewTask, setNewTask] = useState(""); //empty string to store task
+  const [completedTasks, setCompletedTasks] = useState([]); //empty array to store completed task
+  const [inProgress, setInProgress] = useState(0);  //use to count the number of task that are not completed
+  const [countCompleted, setCountCompleted] = useState(0); //use to count the number of task that are completed
 
-  useEffect(() => {
+  //function to count the changes made within tasks and completedTasks
+  useEffect(() => { 
     setInProgress(tasks.length);
     setCountCompleted(completedTasks.length);
   }, [tasks, completedTasks]);
 
+  // function to extract value of the input 
   function handleInputTask(e) {
     setNewTask(e.target.value);
   }
 
+  //function to add task btn
   function handleAddBtn() {
     if (NewTask.trim() !== "") {
       setTasks((prevTasks) => [...prevTasks, NewTask]);
@@ -25,17 +30,21 @@ const ToDo = () => {
     }
   }
 
+
+  //function add task on hitting enter
   function handleKeyDown(e) {
     if (e.key === "Enter") {
       handleAddBtn();
     }
   }
 
+  //function to delete task btn
   function handleDeleteTaskBtn(index) {
     const updatedTasks = tasks.filter((_, i) => i !== index);
     setTasks(updatedTasks);
   }
 
+  //function to done task btn
   function handleDoneBtn(index) {
     const taskToComplete = tasks[index];
     setCompletedTasks((prevCompleted) => [...prevCompleted, taskToComplete]);
@@ -43,15 +52,20 @@ const ToDo = () => {
     setTasks(updatedTasks);
   }
 
+  // function to clear task btn 
   function handleClearbtn() {
     setCompletedTasks([]);
   }
 
   return (
+
+    //main div in which all the content is included
     <div className="m-6 h-screen bg-cover bg-center p-4 shadow-lg rounded-l bg-gray-700">
+      {/* title of the page */}
       <h1 className="text-center mb-4 text-4xl font-bold text-white font-mono">
         To-do List
       </h1>
+      {/* div for input field and add task button  */}
       <div className="flex gap-4 justify-center">
         <input
           type="text"
@@ -68,8 +82,10 @@ const ToDo = () => {
           Add Task
         </button>
       </div>
+
       <hr className="my-6" />
 
+      {/* div to display the status of the tasks  */}
       <div className="w-[30%] px-6 py-3 rounded-2xl font-mono my-4 bg-gray-100">
         <h2 className="font-bold text-2xl blinking-text">Status:</h2>
         <p className="font-semibold">
@@ -83,7 +99,9 @@ const ToDo = () => {
           </span>
         </p>
       </div>
+      {/* main div in which task which are in progress and are completed is shown  */}
       <div className="flex justify-evenly gap-4 max-h-72">
+        {/* div in which task which are in progress in shown  */}
         <div className="shadow-xl px-10 py-5 w-full  rounded-2xl max-h-72 overflow-y-auto bg-gray-100">
           <h2 className="text-xl font-bold bg-red-700 text-center py-2 px-3 rounded-xl text-white font-mono">
             In Progress...
@@ -117,6 +135,7 @@ const ToDo = () => {
           </ol>
         </div>
 
+        {/* div in which the completed task are shown */}
         <div className="shadow-xl px-10 py-5 w-full bg-gray-100 rounded-2xl max-h-72 overflow-y-auto ">
           <h2 className="text-xl font-bold bg-green-700 text-center py-2 px-3 rounded-xl text-white font-mono mb-4">
             Completed...
@@ -134,6 +153,7 @@ const ToDo = () => {
               </li>
             ))}
           </ul>
+          {/* adding functionality to show the clear all button when the completed task have at least one task or else hidden  */}
           <div className="flex">
             {completedTasks < 1 ? (
               <button
